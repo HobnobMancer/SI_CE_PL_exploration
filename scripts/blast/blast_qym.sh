6bdd2c6c9cd5d9fee9091c279895f65feef165ad
HobnobMancer <eemh1@st-andrews.ac.uk>
2023-04-01 13:42:26 +0000

    add scripts to run BLASTP between prot and fam

---
+++ scripts/blast/blast_qym.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+#
+# (c) University of St Andrews 2023
+# (c) University of Strathclyde 2023
+# (c) James Hutton Institute 2023
+#
+# Author:
+# Emma E. M. Hobbs
+#
+# Contact
+# eemh1@st-andrews.ac.uk
+#
+# Emma E. M. Hobbs,
+# Biomolecular Sciences Building,
+# University of St Andrews,
+# North Haugh Campus,
+# St Andrews,
+# KY16 9ST
+# Scotland,
+# UK
+#
+# The MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# blast qYM
+
+# Query QYM prot sequence against CE8, PL1 and PL9 family seqs
+
+mkdir results/blastp/qym/ -p
+
+# generate TSV file
+blastp \
+    -subject data/seqs/ce8.fasta \
+    -query data/seqs/qym_seq.fasta \
+    -out results/blastp/qym/qym_ce8.tsv \
+    -outfmt "6 qseqid sseqid evalue bitscore pident qcovs"
+
+blastp \
+    -subject data/seqs/pl1.fasta \
+    -query data/seqs/qym_seq.fasta \
+    -out results/blastp/qym/qym_pl1.tsv \
+    -outfmt "6 qseqid sseqid evalue bitscore pident qcovs"
+
+blastp \
+    -subject data/seqs/pl9.fasta \
+    -query data/seqs/qym_seq.fasta \
+    -out results/blastp/qym/qym_pl9.tsv \
+    -outfmt "6 qseqid sseqid evalue bitscore pident qcovs"
